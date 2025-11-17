73eed75ce4ea47343ddb87cbbb3ea0a93b437a25
Ayoub-45 <ayoubnasraoui161@yahoo.fr>
2025-11-17 22:19:08 +0100

    add app

---
+++ server.js
@@ -0,0 +1,8 @@
+import express from "express"
+import { configDotenv } from "dotenv"
+configDotenv()
+const app = express()
+const PORT=process.env.PORT || 5000
+app.listen(PORT,() => {
+    console.log(`App is running on port ${PORT}`)
+})
